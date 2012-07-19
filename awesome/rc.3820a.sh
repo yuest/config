5c3012c009ef1b4b3a47559d801f94978687bcb4
Yuest Wang <yuestwang@gmail.com>
2012-07-19 19:38:12 +0800

    / ‘awesome/rc.3820a.sh’

--- awesome/rc.3820a.sh
+++ awesome/rc.3820a.sh
@@ -1,7 +1,6 @@
 ibus-daemon -d &
-nautilus -n &
 start-pulseaudio-x11 &
 start-pulseaudio-kde &
-dropbox start
+nautilus -n ; dropboxd &
 sparkleshare start
 sleep 0.1; xinput set-int-prop 'AT Translated Set 2 keyboard' 'Device Enabled' 8 0
