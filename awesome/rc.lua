7c33d9445a4951dcd874752c5f49b4eb2390c7e1
Yuest Wang <yuestwang@gmail.com>
2012-07-27 22:21:37 +0800

    / ‘awesome/rc.lua’

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -48,7 +48,7 @@ layouts =
 tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[3])
+    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[2])
 end
 -- }}}
 
