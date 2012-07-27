8a3cd3a75845fcc6a663e2a78ba215b191702c64
Yuest Wang <yuestwang@gmail.com>
2012-07-27 23:14:54 +0800

    / ‘awesome/rc.lua’

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -172,7 +172,7 @@ root.buttons(awful.util.table.join(
 globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "minus",  function () awful.util.spawn("vol_down") end),
     awful.key({ modkey,           }, "equal",  function () awful.util.spawn("vol_up") end),
-    awful.key({ modkey, "Shift"   }, "minus",  function () awful.util.spawn("vol_up") end),
+    awful.key({ modkey, "Shift"   }, "minus",  function () awful.util.spawn("mute_toggle") end),
     awful.key({ modkey,           }, "v",      rodentbane.start         ),
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev       ),
     awful.key({ modkey,           }, "Right",  awful.tag.viewnext       ),
