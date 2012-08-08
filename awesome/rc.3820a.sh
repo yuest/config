ebcf1f3234b522fb849f5256aa780d19224555a5
Yuest Wang <yuestwang@gmail.com>
2012-08-08 21:18:30 +0800

    / ‘awesome/rc.3820a.sh’

--- awesome/rc.3820a.sh
+++ awesome/rc.3820a.sh
@@ -4,5 +4,5 @@ start-pulseaudio-kde &
 echo 100000 | sudo tee /proc/sys/fs/inotify/max_user_watches
 nautilus -n ; dropboxd &
 sparkleshare start
-sleep 0.1; xinput set-int-prop 'AT Translated Set 2 keyboard' 'Device Enabled' 8 0
+#sleep 0.1; xinput set-int-prop 'AT Translated Set 2 keyboard' 'Device Enabled' 8 0
 sudo /etc/rc.d/wicd start
