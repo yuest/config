c07226fba53b3f541028a42621a5be6f19719da8
Yuest Wang <yuestwang@gmail.com>
2012-08-15 01:31:05 +0800

    + ‘awesome/rc.worka.sh’

---
+++ awesome/rc.worka.sh
@@ -0,0 +1,9 @@
+#ibus-daemon -d &
+start-pulseaudio-x11 &
+start-pulseaudio-kde &
+echo 100000 | sudo tee /proc/sys/fs/inotify/max_user_watches
+nautilus -n ; dropboxd &
+sparkleshare start
+#sleep 0.1; xinput set-int-prop 'AT Translated Set 2 keyboard' 'Device Enabled' 8 0
+#sudo /etc/rc.d/wicd start
+#setxkbmap -option ctrl:swapcaps
